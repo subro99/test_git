c4c8d36f89489d1411259d66746810ae6f89d351
subro <sebek.kanatek45@gmail.com>
2022-03-07 17:20:38 +0000

    clearing test.py

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
 print("xD")
 print("test git fetch")
-#zmiana 2
-#zmiana 3
-
-#working issue1
-#working issue1.2
-
-#working on master without commit
-#work on issue1 with commit
-#working issue1 only stage
